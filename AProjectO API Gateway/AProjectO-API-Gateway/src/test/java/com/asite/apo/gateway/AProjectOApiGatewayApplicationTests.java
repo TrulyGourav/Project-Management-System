5352fab1688d9f8621d1ee8cac4e618e912088a6
trulyGourav <gourav.oct@gmail.com>
2023-05-04 06:15:12 +0530

    API Gateway updated

---
+++ AProjectO API Gateway/AProjectO-API-Gateway/src/test/java/com/asite/apo/gateway/AProjectOApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asite.apo.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AProjectOApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
